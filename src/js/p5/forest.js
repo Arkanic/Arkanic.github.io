04b22d02e00f6ebba99661c42ec20cb0ee7cf5b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-11 04:50:41 +0000

    Deploy Arkanic/Arkanic.github.io to github.com/Arkanic/Arkanic.github.io.git:gh-pages

--- src/js/p5/forest.js
+++ src/js/p5/forest.js
@@ -7,8 +7,10 @@ function setup() {
 	mxt = random(1, windowWidth);
 	myt = random(1, windowHeight)
 }
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
 function draw() {
-
     background(255);
 	textSize(s)
 	c += 2e-4//2e-4;
