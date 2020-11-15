574373963dbe9ed29d071d56f1c154b211472b2c
Arkanic <silverfishasaurus@gmail.com>
2020-11-15 23:33:11 +0000

    more thing

--- sitemap.sh
+++ sitemap.sh
@@ -1,3 +1,4 @@
+cd site
 find . -type f -not -path "./.git/*" > sitemap-temp.txt
 stdbuf -o0 cut -c2- sitemap-temp.txt > sitemap-temp2.txt
 for i in $(cat sitemap-temp2.txt); do echo "https://arkanic.github.io$i"; done > sitemap-temp3.txt
