315cbf60ea2754cbb723f896dded83820bcc8167
Arkanic <silverfishasaurus@gmail.com>
2020-11-16 23:07:02 +0000

    things

---
+++ tools/generateP5List.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+let exportFiles = [];
+fs.readdir(__dirname + "../site/projects/webtoys/js/p5/", (err, files) => {
+    if(err) throw err;
+    files.forEach(file => {
+        exportFiles.push(file);
+    });
+    let exportAssetNamesString = "module.exports=";
+    exportAssetNamesString += JSON.stringify(exportFiles);
+    exportAssetNamesString = new Uint8Array(Buffer.from(exportAssetNamesString));
+    fs.writeFile(__dirname + "../site/projects/webtoys/js/generated/p5List.js", exportAssetNamesString, err => {
+        if(err) throw err;
+        console.log("Generated p5.js webtoy list.");
+    });
+});
