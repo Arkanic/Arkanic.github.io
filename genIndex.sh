b5225bece68109c8504cb7bf6365a997844e9ae5
Arkanic <silverfishasaurus@gmail.com>
2021-04-22 23:21:34 +0000

    fix error

--- genIndex.sh
+++ genIndex.sh
@@ -5,9 +5,10 @@ HTTP="/"
 OUTPUT="./site/notes/index.html" 
 
 i=0
-echo "<ul>" > $OUTPUT
+echo "<!DOCTYPE html><html><head><title>Notes Index</title></head><body><ul>" > $OUTPUT
 for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
   path=`basename "$filepath"`
-  echo "  <li><a href=\"/$path/\">$path</a></li>" >> $OUTPUT
+  formattedpath="${path//-/ }"
+  echo "  <li><a href=\"/$path/\">$formattedpath</a></li>" >> $OUTPUT
 done
-echo "</ul>" >> $OUTPUT
+echo "</ul></body></html>" >> $OUTPUT
