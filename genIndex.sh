f1bf9c4c656b0e6d3c062e28c994a1087fe8bd52
Arkanic <silverfishasaurus@gmail.com>
2021-07-16 21:47:21 +0000

    index styling test

--- genIndex.sh
+++ genIndex.sh
@@ -6,7 +6,7 @@ OUTPUT="./site/projects/index.html"
 OUTPUTJSON="./site/projects/index.json"
 
 i=0
-echo "<!DOCTYPE html><html><head><title>Projects Index</title></head><body><ul>" > $OUTPUT
+echo "<!DOCTYPE html><html><head><title>Projects Index</title><link rel=\"stylesheet\" type=\"text/css\" href=\"../src/css/index.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"../src/css/global.css\"></head><body><ul>" > $OUTPUT
 echo '{"files":[' > $OUTPUTJSON
 for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
   path=`basename "$filepath"`
