99072e86ff2d30eb586dd93003153481b052e1b7
Arkanic <silverfishasaurus@gmail.com>
2021-07-16 21:51:17 +0000

    standard page

--- genIndex.sh
+++ genIndex.sh
@@ -6,7 +6,7 @@ OUTPUT="./site/projects/index.html"
 OUTPUTJSON="./site/projects/index.json"
 
 i=0
-echo "<!DOCTYPE html><html><head><title>Projects Index</title><link rel=\"stylesheet\" type=\"text/css\" href=\"../src/css/index.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"../src/css/global.css\"></head><body><ul>" > $OUTPUT
+echo "<!--#include virtual=\"components/top.html\"--><title>Projects Index</title><!--#include virtual=\"components/top2.html\"--><ul>" > $OUTPUT
 echo '{"files":[' > $OUTPUTJSON
 for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
   path=`basename "$filepath"`
@@ -14,6 +14,6 @@ for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
   echo "<li><a href=\"./$path/\">$formattedpath</a></li>" >> $OUTPUT
   echo "{\"name\":\"${formattedpath}\",\"location\":\"./${path}/\"}," >> $OUTPUTJSON
 done
-echo "</ul></body></html>" >> $OUTPUT
+echo "</ul><!--#include virtual=\"components/bottom.html\"-->" >> $OUTPUT
 truncate -s-2 $OUTPUTJSON
 echo "]}" >> $OUTPUTJSON
